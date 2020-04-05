2fa94dc089c086d027a465b53bb004156509607e
yadu <yaduhat@gmail.com>
2020-04-04 19:33:26 -0500

    first commit

---
+++ src/main/java/miu/edu/cs/ea/project/miu_onlineshopping/miuOnlineShopping/App.java
@@ -0,0 +1,13 @@
+package miu.edu.cs.ea.project.miu_onlineshopping.miuOnlineShopping;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
